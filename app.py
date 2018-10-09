24a476ffdd38a310118fe31d0ab4a063026ec3e7
NesamaniRajamanickam <43475324+NesamaniRajamanickam@users.noreply.github.com>
2018-10-09 18:46:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,129 +1,107 @@
 import dash
-import plotly
-import dash_html_components as html
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
-import dash_table_experiments as dte
-from dash.dependencies import Input, Output, State
-
+import dash_html_components as html
+import dash_table_experiments as dt
+import json
 import pandas as pd
 import numpy as np
-
-import json
-import datetime
-import operator
-import os
-
-import base64
-import io
-
-
+import plotly
 
 app = dash.Dash()
 
-app.scripts.config.serve_locally = True
-app.config['suppress_callback_exceptions'] = True
+app.scripts.config.serve_locally=True
+
+DF_WALMART = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/1962_2006_walmart_store_openings.csv')
+
+DF_GAPMINDER = pd.read_csv(
+    'https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv'
+)
+DF_GAPMINDER = DF_GAPMINDER[DF_GAPMINDER['year'] == 2007]
+
+DF_SIMPLE = pd.DataFrame({
+    'x': ['A', 'B', 'C', 'D', 'E', 'F'],
+    'y': [4, 3, 1, 2, 3, 6],
+    'z': ['a', 'b', 'c', 'a', 'b', 'c']
+})
 
 app.layout = html.Div([
-
-    html.H5("Upload Files"),
-    dcc.Upload(
-        id='upload-data',
-        children=html.Div([
-            'Drag and Drop or ',
-            html.A('Select Files')
-        ]),
-        style={
-            'width': '100%',
-            'height': '60px',
-            'lineHeight': '60px',
-            'borderWidth': '1px',
-            'borderStyle': 'dashed',
-            'borderRadius': '5px',
-            'textAlign': 'center',
-            'margin': '10px'
-        },
-        multiple=False),
-    html.Br(),
-    html.Button(
-        id='propagate-button',
-        n_clicks=0,
-        children='Propagate Table Data'
+    html.H4('Gapminder DataTable'),
+    dt.DataTable(
+        rows=DF_GAPMINDER.to_dict('records'),
+        filterable=False,
+        sortable=True,
+        id='datatable-gapminder'
+    ),
+    dcc.Graph(
+        id='graph-gapminder'
     ),
 
+    html.H4('Simple DataTable'),
+    dt.DataTable(
+        rows=DF_SIMPLE.to_dict('records'),
+        filterable=False,
+        sortable=True,
+        id='datatable'
+    ),
+    dcc.Graph(
+        id='graph'
+    ),
+], className="container")
 
-    html.Br(),
-    html.H5("Filter Column"),
-    dcc.Dropdown(id='dropdown_table_filterColumn',
-        multi = False,
-        placeholder='Filter Column'),
+@app.callback(
+    Output('graph', 'figure'),
+    [Input('datatable', 'rows')])
+def update_figure(rows):
+    dff = pd.DataFrame(rows)
+    return {
+        'data': [{
+            'x': dff['x'],
+            'y': dff['y'],
+            'text': dff['z'],
+            'type': 'bar'
+        }]
+    }
 
+@app.callback(
+    Output('graph-gapminder', 'figure'),
+    [Input('datatable-gapminder', 'rows')])
+def update_figure(rows):
+    dff = pd.DataFrame(rows)
+    fig = plotly.tools.make_subplots(
+        rows=3, cols=1,
+        subplot_titles=('Life Expectancy', 'GDP Per Capita', 'Population',),
+        shared_xaxes=True)
+    marker = {'color': '#0074D9'}
+    fig.append_trace({
+        'x': dff['country'],
+        'y': dff['lifeExp'],
+        'type': 'bar',
+        'marker': marker
+    }, 1, 1)
+    fig.append_trace({
+        'x': dff['country'],
+        'y': dff['gdpPercap'],
+        'type': 'bar',
+        'marker': marker
+    }, 2, 1)
+    fig.append_trace({
+        'x': dff['country'],
+        'y': dff['pop'],
+        'type': 'bar',
+        'marker': marker
+    }, 3, 1)
+    fig['layout']['showlegend'] = False
+    fig['layout']['height'] = 800
+    fig['layout']['margin'] = {
+        'l': 20,
+        'r': 20,
+        't': 60,
+        'b': 200
+    }
+    return fig
 
-    html.Br(),
-    html.H5("Updated Table"),
-    html.Div(dte.DataTable(rows=[{}], id='table'))
-
-
-])
-
-
-# Functions
-
-# file upload function
-def parse_contents(contents, filename):
-    content_type, content_string = contents.split(',')
-
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-        elif 'xls' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-
-    except Exception as e:
-        print(e)
-        return None
-
-    return df
-
-
-# callback table creation
-@app.callback(Output('table', 'rows'),
-              [Input('upload-data', 'contents'),
-               Input('upload-data', 'filename')])
-def update_output(contents, filename):
-    if contents is not None:
-        df = parse_contents(contents, filename)
-        if df is not None:
-            return df.to_dict('records')
-        else:
-            return [{}]
-    else:
-        return [{}]
-
-
-#callback update options of filter dropdown
-@app.callback(Output('dropdown_table_filterColumn', 'options'),
-              [Input('propagate-button', 'n_clicks'),
-               Input('table', 'rows')])
-def update_filter_column_options(n_clicks_update, tablerows):
-    if n_clicks_update < 1:
-        print ("df empty")
-        return []
-
-    else:
-        dff = pd.DataFrame(tablerows) # <- problem! dff stays empty even though table was uploaded
-
-        print("updating... dff empty?:"), dff.empty #result is True, labels stay empty
-
-        return [{'label': i, 'value': i} for i in sorted(list(dff))]
-
-
-app.css.append_css({
-    "external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
-})
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
